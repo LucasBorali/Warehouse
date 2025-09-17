ec7af41f3a767dc5d33b986bf2a47d9cfba571af
Administrador <Administrador@5CG9216CBP>
2025-09-17 18:42:13 -0300

    OutputVM funcional, agora é seguir para as verificações, filtros e funções mais específicas

--- ViewModels/OutputVM.cs
+++ ViewModels/OutputVM.cs
@@ -1,27 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Warehouse.Models;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Windows;
+using Warehouse.Models;
+using Warehouse.DB;
 
 namespace Warehouse.ViewModels
 {
-    public partial class OutputVM: ObservableObject
+    public partial class OutputVM : ObservableObject
     {
-        public OutputVM() {
+        public OutputVM()
+        {
             var context = new DB.AppDbContext();
             context.Database.EnsureCreated();
 
             _ = LoadInputLogs();
+            _ = LoadOutputLogs();
 
         }
 
         [ObservableProperty]
         private ObservableCollection<Input> inputLogs = new();
+
+        [ObservableProperty]
+        private ObservableCollection<Output> outputLogs = new();
 
         [ObservableProperty]
         private ObservableCollection<string> producers = new();
@@ -43,6 +45,16 @@ namespace Warehouse.ViewModels
             Producers = new ObservableCollection<string>(InputLogs.Select(i => i.Producer).Distinct());
 
 
+        }
+
+        private async Task LoadOutputLogs()
+        {
+            var docks = await DB.DbExtensions.GetAllAsync<Output>();
+            OutputLogs.Clear();
+            foreach (var dock in docks)
+            {
+                OutputLogs.Add(dock);
+            }
         }
 
         [ObservableProperty]
@@ -67,7 +79,60 @@ namespace Warehouse.ViewModels
             }
         }
 
-      
+        [ObservableProperty]
+        private DateTime date = DateTime.Now;
+
+        [ObservableProperty]
+        private string plate;
+
+        [ObservableProperty]
+        private string peso;
+
+        [ObservableProperty]
+        private string buyer;
+
+
+
+
+        [RelayCommand]
+        private async Task AddOutputLog()
+        {
+            if (SelectedProducer == null || Selectedticket == null || string.IsNullOrWhiteSpace(Plate) || string.IsNullOrWhiteSpace(Peso) || string.IsNullOrWhiteSpace(Buyer))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos.");
+                return;
+            }
+            var finalDate = Date.Date == DateTime.Today ? DateTime.Now : Date;
+
+            var newOutput = new Output
+            {
+                Date = finalDate,
+                Ticket = Selectedticket,
+                Plate = Plate,
+                Peso = Peso,
+                Buyer = Buyer
+            };
+            await DbExtensions.AddAsyncData(newOutput);
+            await LoadOutputLogs();
+            await LoadInputLogs();
+
+            MessageBox.Show("Saída registrada com sucesso!");
+            ClearFields();
+        }
+
+
+        private void ClearFields()
+        {
+            SelectedProducer = string.Empty;
+            Selectedticket = string.Empty;
+            Plate = string.Empty;
+            Peso = string.Empty;
+            Buyer = string.Empty;
+            Date = DateTime.Now;
+        }
+
+
+
 
 
 
